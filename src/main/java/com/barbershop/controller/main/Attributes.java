7c20be61cdea9e3a36b21a9a72521dc4f95b7883
Kirill Kuzmich <kirill.kuzmich.03@mail.ru>
2023-04-17 15:10:37 +0300

    Master page

--- src/main/java/com/barbershop/controller/main/Attributes.java
+++ src/main/java/com/barbershop/controller/main/Attributes.java
@@ -21,6 +21,44 @@ public class Attributes extends Main {
         model.addAttribute("users", usersRepo.findAll());
         model.addAttribute("roles", Role.values());
     }
+    protected void AddAttributesNoteAdd(Model model) {
+        AddAttributes(model);
+        model.addAttribute("categories", categoryRepo.findAll());
+    }
+
+    protected void AddAttributesNoteEdit(Model model, Long id) {
+        AddAttributes(model);
+        model.addAttribute("categories", categoryRepo.findAll());
+        model.addAttribute("note", notesRepo.getReferenceById(id));
+    }
+
+    protected void AddAttributesNotesMy(Model model) {
+        AddAttributes(model);
+        Users user = getUser();
+        if (user.getRole() == Role.MASTER) {
+            model.addAttribute("orderings", user.getMaster().getOrderings());
+        } else {
+            model.addAttribute("orderings", user.getOrderings());
+        }
+    }
+
+    protected void AddAttributesNote(Model model, Long id) {
+        AddAttributes(model);
+        Notes note = notesRepo.getReferenceById(id);
+        List<Masters> masters = mastersRepo.findAllByCategory(note.getCategory().getName());
+        model.addAttribute("note", note);
+        model.addAttribute("masters", masters);
+    }
+
+    protected void AddAttributesMaster(Model model) {
+        AddAttributes(model);
+        model.addAttribute("categories", categoryRepo.findAll());
+    }
+
+    protected void AddAttributesStats(Model model) {
+        AddAttributes(model);
+        model.addAttribute("notes", notesRepo.findAll());
+    }
 
     protected void AddAttributesIndex(Model model) {
         AddAttributes(model);
