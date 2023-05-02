c2fe5569994f2e0249fda4d4ffbd9903bb5049df
Kirill Kuzmich <kirill.kuzmich.03@mail.ru>
2023-05-02 10:53:58 +0300

    finish commit. Added pages: about and stats

---
+++ src/main/java/com/barbershop/controller/AboutCont.java
@@ -0,0 +1,17 @@
+package com.barbershop.controller;
+
+import com.barbershop.controller.main.Attributes;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/about")
+public class AboutCont extends Attributes {
+    @GetMapping
+    public String Category(Model model) {
+        AddAttributes(model);
+        return "about";
+    }
+}
